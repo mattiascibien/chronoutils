7afc7d486cc008208c534a1894bec04e5818cca9
Mattias Cibien <mattias.cibien@outlook.com>
2017-01-19 11:02:14 +0100

    Initial Code

---
+++ src/ChronoUtils/Chrono.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ChronoUtils
+{
+    public static class Chrono
+    {
+        public static TimeSpan MeasureTime(Action action)
+        {    
+            Stopwatch sw = Stopwatch.StartNew();
+            action();
+            sw.Stop();
+            return sw.Elapsed;
+        }
+
+        public static T MeasureTime<T>(Func<T> func, out TimeSpan elapsed)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            var result = func();
+            elapsed = sw.Elapsed;
+            return result;
+        }
+    }
+}
---
+++ test/ChronoUtils.Tests/ChronoTests.cs
@@ -0,0 +1,14 @@
+﻿using ChronoUtils;
+using Xunit;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1() 
+        {
+            Assert.True(true);
+        }
+    }
+}
